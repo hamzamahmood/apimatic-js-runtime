60bb664c1b9ddbccc27b3d10ff6030274b92cb70
Mehdi Jaffery <mehdi.jaffery@apimatic.io>
2020-10-19 00:49:01 +0500

    Adds dictionary mapping with XML entry elements.

--- packages/schema/src/types/dict.ts
+++ packages/schema/src/types/dict.ts
@@ -27,6 +27,7 @@ export function dict<T, S>(
       (v, childCtxt) => itemSchema[validateFn](v[1], childCtxt)
     );
   };
+
   return {
     type: `Record<string,${itemSchema.type}>`,
     validateBeforeMap: (...args) => validate('validateBeforeMap', ...args),
@@ -90,4 +91,99 @@ export function dict<T, S>(
       return output;
     },
   };
+}
+
+export function dictWithXmlEntries<T, S>(
+  itemSchema: Schema<T, S>
+): Schema<Record<string, T>, Record<string, S>> {
+  const dictSchema = dict(itemSchema);
+  const modifiedSchema = { ...dictSchema };
+
+  modifiedSchema.unmapXml = (value, ctxt) => {
+    const output: Record<string, S> = dictSchema.unmapXml(value, ctxt);
+
+    // Convert each entry to XML "entry" elements. The XML "entry" element looks
+    // like this: `<entry key="key">value</entry>`. Note that the element name
+    // "entry" is set later at the return.
+    const entries = objectEntries(output).map(([key, value]) => ({
+      $: { key },
+      _: value,
+    }));
+
+    return { entry: entries };
+  };
+
+  modifiedSchema.mapXml = (value, ctxt) => {
+    // Empty dictionary
+    if (!('entry' in value)) {
+      return {};
+    }
+
+    let { entry: entries } = value as {
+      entry: { $: { key: string }; _: unknown }[];
+    };
+
+    // For single entry, the XML parser gives a single object instead of an array.
+    // Make it an array for easier handling.
+    if (!Array.isArray(entries)) {
+      entries = [entries];
+    }
+
+    // Convert entry elements containing a key attribute and content to a dictionary.
+    const dictObj: Record<string, unknown> = {};
+    for (const item of entries) {
+      dictObj[item.$.key] = item._;
+    }
+
+    // Run validation on entry values against the item schema.
+    // TODO: Maintain context and path when delegating validatin
+    return dictSchema.mapXml(dictObj, ctxt);
+  };
+
+  modifiedSchema.validateBeforeMapXml = (value, ctxt) => {
+    if (typeof value !== 'object' || value === null) {
+      return ctxt.fail();
+    }
+
+    // Empty dictionary case
+    if (!('entry' in value)) {
+      return [];
+    }
+
+    let entries = (value as { entry: object[] })['entry'];
+
+    // Non-repeating XML elements are passed as a single-object instead of an array of objects.
+    // We normalize this behavior.
+    if (!Array.isArray(entries)) {
+      entries = [entries];
+    }
+
+    // Dictionary for all entries
+    const dictObj: Record<string, unknown> = {};
+
+    for (let index = 0; index < entries.length; index++) {
+      const entry = entries[index];
+      // Fail if entry is not an XML element object.
+      if (typeof entry !== 'object' || entry === null) {
+        return ctxt.fail('Expected "entry" to be an XML element.');
+      }
+
+      // Fail if entry does not have an attribute named key.
+      if (!('$' in entry) || !('key' in (entry as { $: object }).$)) {
+        return ctxt.fail(
+          'Expected "entry" element to have an attribute named "key".'
+        );
+      }
+
+      // Set entry in dictionary
+      const typedEntry = entry as { $: { key: string }; _: unknown };
+      dictObj[typedEntry.$.key] = typedEntry._;
+    }
+
+    // Check all entry values against item schema.
+    // TODO: Maintain context and path when delegating validatin
+    return dictSchema.validateBeforeMapXml(dictObj, ctxt);
+  };
+
+  return modifiedSchema;
 }
