7c6f91186d67bad657d0e78f20531fea99b6bcca
MaryamAdnan <80243792+MaryamAdnan3@users.noreply.github.com>
2023-02-17 16:31:54 +0500

    ci(typescript): update readme.md, license.md and dependency-review.yml (#96)
    Closes #95

--- packages/http-query/jest.config.js
+++ packages/http-query/jest.config.js
@@ -3,4 +3,5 @@ const { jest: lernaAliases } = require('lerna-alias');
 module.exports = {
   preset: 'ts-jest',
   moduleNameMapper: lernaAliases(),
+  coverageReporters: [['lcov', { projectRoot: '../../' }]]
 };
